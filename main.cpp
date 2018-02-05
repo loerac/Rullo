9ed750dd271e6113780d031eb7d45699bc32823a
Christian Loera <chatoloera18@gmail.com>
2018-02-05 02:45:06 -0600

    Using combinatorics to solve rullo puzzle

--- main.cpp
+++ main.cpp
@@ -1,131 +1,106 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
+ 
+/* Type marker stick: using bits to indicate what's chosen.  The stick can't
+ * handle more than 32 items, but the idea is there; at worst, use array instead */
+typedef unsigned long marker;
+marker one = 1;
 
-// Linked list
-// Allows to insert and delete easily
 struct node {
-	int data;
-	struct node* next;
+    int value;
+    struct node *next;
 };
-struct node* head = NULL;
+struct node *head = NULL;
 
-// Functions
-void insert(int data);
-void deleteLast();
-void display();
-void print();
+void comb(int size, int curr, int list[], int rullo, marker chosen, int at);
+void insertFirst(int value);
+void deleteFirst();
+void printList();
 int sum();
 int length();
 
-int main(int argc, char** argv) {
-	// Checking if there are arguements given
-	if(argc < 3) {
-		printf("\nError: no arguements given: %s [result] [single row/column]\n\n", argv[0]);
-		return 2;
-	}
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        printf("Error: No arguments given: %s [rullo number] [set of row/column]\n", argv[0]);
+        return 2;
+    }
 
-	int volatile i = 0, data = 0;
-	int index = 0;		// Increments up the list[]
-	int pred = -1;		// The last successful predecessor that will increment
-	int rullo = atoi(argv[1]);
-	int size = 0; 
-	int const SIZE = (argc - 2);
-	int list[SIZE];
+    int i = 0;
+    int rullo = atoi(argv[1]);
 
-	for(i = 0; i < (argc - 2); i++) {
-		if(atoi(argv[i + 2]) <= rullo) {
-			list[index] = atoi(argv[i + 2]);
-			index++;
-			size++;
-		}
-	}
-	
-	printf("Result given: %d\n", rullo);
-	printf("Row/Column given: [");
-	for(i = 0; i < index; i++) { printf(" %d", list[i]); }
-	printf(" ]\n=====================================\n\n");
-	
-	index = 0;
-	for(i = 0; i < SIZE; i++) {
-		while(pred != size && pred != SIZE) {
-			//printf(">> %d\n", list[index]);
-			insert(list[index]);
-			//print();
-			index++;
-			if(sum() == rullo) { 
-				display();
-				deleteLast();
-			}
-			else if(sum() > rullo) { deleteLast(); }
-			else { pred++; }
-		
-			printf("--> %d %d\n", index, pred);
-			if(index >= size) {
-				deleteLast();
-				index = (length() == 1 ? pred : (pred + 1));
-			}
-		}
-		for(int j = 0; j < length(); j++) { deleteLast(); }
-		size--;
-		pred = (i + 1);
-		index = pred;
-		printf("----> %d %d\n", index, pred);
-		//printf("%d %d %d\n", size, pred, index);
-	}
-	return 0;
+    for(i = 2; i < argc; i++) {
+        if(atoi(argv[i]) <= rullo) { insertFirst(atoi(argv[i])); }
+    }
+
+    int const SIZE = length();
+    int list[SIZE];
+    for(i = 0; i < SIZE; i++) {
+        list[i] = head->value;
+        head = head->next;
+    }
+    while(length()) { deleteFirst(); }
+
+    printf("Rullo number: %d\n", rullo);
+    printf("Accepted set: [ ");
+    for(i = 0; i < SIZE; i++) { printf("%d ", list[i]); }
+    printf("]\n==========================================\n");
+    for(i = 0; i < (SIZE + 1); i++) { comb(SIZE, i, list, rullo, 0, 0); }
+    printf("\n");
+    return 0;
 }
 
-// Insert data to the list
-void insert(int data) {
-	struct node* link = (struct node*) malloc(sizeof(struct node));
-	link->data = data;
-	link->next = head;
-	head = link;
+// Implementing the combinatorics code from Rosetta Code
+// http://rosettacode.org/wiki/Combinations#C.2B.2B
+void comb(int size, int curr, int list[], int rullo, marker chosen, int i) {
+    if (size < (curr + i)) { return; } /* not enough bits left */
+ 
+    if (!curr) {
+        /* got all we curred; print the thing.  if other actions are
+         * desired, we could have passed in a callback function. */
+        for(i = 0; i < size; i++) { if (chosen & (one << i)) { insertFirst(list[i]); } }
+        if(sum() == rullo) { printList(); }
+        while(length()) { deleteFirst(); }
+        return;
+    }
+    /* if we choose the current item, "or" (|) the bit to mark it so. */
+    comb(size, curr - 1, list, rullo, chosen | (one << i), i + 1);
+    comb(size, curr, list, rullo, chosen, i + 1);  /* or don't choose it, go to next */
+}
+ 
+void insertFirst(int value) {
+    struct node* temp = (struct node*) malloc(sizeof(struct node));
+    temp->value = value;
+    temp->next = head;
+    head = temp;
 }
 
+void deleteFirst() { head = head->next; }
+/*    struct node *temp= head;
+    temp = temp->next;
+    head = temp;
+}*/
 
-// Delete the last node entered to the list
-void deleteLast() { head = head->next; }
+void printList() {
+    struct node *temp = head;
 
-// Displaying the found combination
-void display() {
-	struct node* temp = head;
-
-	printf("Found Combination: [ ");
-	while(temp != NULL) {
-		printf("%d ", temp->data);
-		temp = temp->next;
-	}
-	printf("]\n");
+    printf("Found combination: [ ");
+    while(temp != NULL) {
+        printf("%d ", temp->value);
+        temp = temp->next;
+    }
+    printf("]\n");
 }
 
-void print() {
-	struct node* temp = head;
-
-	printf("--> ");
-	while(temp != NULL) {
-		printf("%d ", temp->data);
-		temp = temp->next;
-	}
-	printf("\n");
-}
-
-// Finding the sum of the list
 int sum() {
-	int sum = 0;
-	struct node* temp;
-
-	for(temp = head; temp != NULL; temp = temp->next) { sum += temp->data; }
-	return sum;
+    int sum = 0;
+    struct node *temp;
+    for(temp = head; temp != NULL; temp = temp->next) { sum += temp->value; }
+    return sum;
 }
 
-// Getting the length of the list
 int length() {
-	int length = 0;
-	struct node* temp;
-	
-	for(temp = head; temp != NULL; temp = temp->next) { length++; }
-	return length;
+    int length = 0;
+    struct node *temp;
+    for(temp = head; temp != NULL; temp = temp->next) { length++; }
+    return length;
 }
