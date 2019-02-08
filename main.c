573266aaa51ae3834a4f6ed09c6aecf89399df7a
Christian Loera <chatoloera@gmail.com>
2019-02-08 17:40:10 -0600

    Update my poopy code

--- main.c
+++ main.c
@@ -1,83 +1,123 @@
-#include<stdio.h>
-#include<stdlib.h>
- 
-/* Type marker stick: using bits to indicate what's chosen.  The stick can't
- * handle more than 32 items, but the idea is there; at worst, use array instead */
-typedef unsigned long marker;
-marker one = 1;
+#include <errno.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
-struct node {
-    int value;
-    struct node *next;
-};
-struct node *head = NULL;
+unsigned long one = 1;
 
-void comb(int size, int curr, int list[], int rullo, marker chosen, int at);
-void insertFirst(int value);
-void deleteFirst();
-void printList();
-int sum();
-int length();
+typedef struct complete_set_struct {
+    uint32_t value;
+    struct complete_set_struct *next;
+} set_s;
 
-int main(int argc, char **argv) {
-    if(argc < 3) {
-        printf("Error: No arguments given: %s [rullo number] [set of row/column]\n", argv[0]);
-        return 2;
+static set_s *head = NULL;
+static uint32_t set_length = 0;
+
+/********************************************************
+ * INPUTS:
+ *      val_str - The string value integer
+ *      val_int - The output for the string value converted
+ *
+ * OUTPUTS:
+ *      val_int - The string value converted to an
+ *                unsigned int
+ * RETURN:
+ *       true - Success
+ *      false - Failure
+ *
+ * DeSCRIPTION:
+ *      Converts a string to an unsigned integer.
+ ********************************************************/
+static bool ok_strtol(const char *const val_str, uint32_t *val_int) {
+    bool ok = false;
+    char *endptr = NULL;
+    uint32_t val = (uint32_t)strtol(val_str, &endptr, 10);
+
+    if (    (errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) ||
+            (errno != 0 && val == 0)) {
+        printf("Error: Failed on converting string to usigned int - string(%s) error(%m)\n", val_str);
+    } else if (endptr == val_str) {
+        printf("Error: No digits were found - string(%s)\n", val_str);
+    } else {
+        ok = true;
+        *val_int = val;
     }
 
-    int i = 0;
-    int rullo = atoi(argv[1]);
+    return ok;
+}
 
-    for(i = 2; i < argc; i++) {
-        if(atoi(argv[i]) <= rullo) { insertFirst(atoi(argv[i])); }
+/********************************************************
+ * INPUTS:
+ *      value - Data being added to the list
+ *
+ * OUTPUTS:
+ *      head - The data being inserted prepended to the
+ *             linked list
+ *
+ * RETURN:
+ *      NONE
+ *
+ * DeSCRIPTION:
+ *      See INPUTS
+ ********************************************************/
+static bool insertFirst(const uint32_t value) {
+    bool ok = true;
+    set_s* temp = (set_s*)calloc(1, sizeof(set_s));
+
+    if (NULL != temp) {
+        temp->value = value;
+        temp->next = head;
+        head = temp;
+        ++set_length;
+    } else {
+        printf("Error: Allocating memory failed - error(%m)\n");
+        ok = false;
     }
 
-    int const SIZE = length();
-    int list[SIZE];
-    for(i = 0; i < SIZE; i++) {
-        list[i] = head->value;
+    return ok;
+}
+
+/********************************************************
+ * INPUTS:
+ *      NONE
+ *
+ * OUTPUTS:
+ *      head - The first item in the list being removed
+ *
+ * RETURN:
+ *      NONE
+ *
+ * DeSCRIPTION:
+ *      Removes the first item in the linked list. Can be
+ *      called in a loop to remove all items in the list.
+ ********************************************************/
+static void deleteFirst( void ) {
+    set_s *temp = head;
+    if (NULL != temp) {
         head = head->next;
+        free(temp);
+        --set_length;
     }
-    while(length()) { deleteFirst(); }
-
-    printf("Rullo number: %d\n", rullo);
-    printf("Accepted set: [ ");
-    for(i = 0; i < SIZE; i++) { printf("%d ", list[i]); }
-    printf("]\n==========================================\n");
-    for(i = 0; i < (SIZE + 1); i++) { comb(SIZE, i, list, rullo, 0, 0); }
-    printf("\n");
-    return 0;
 }
 
-// Implementing the combinatorics code from Rosetta Code
-// http://rosettacode.org/wiki/Combinations#C.2B.2B
-void comb(int size, int curr, int list[], int rullo, marker chosen, int i) {
-    if (size < (curr + i)) { return; } /* not enough bits left */
- 
-    if (!curr) {
-        /* got all we curred; print the thing.  if other actions are
-         * desired, we could have passed in a callback function. */
-        for(i = 0; i < size; i++) { if (chosen & (one << i)) { insertFirst(list[i]); } }
-        if(sum() == rullo) { printList(); }
-        while(length()) { deleteFirst(); }
-        return;
-    }
-    /* if we choose the current item, "or" (|) the bit to mark it so. */
-    comb(size, curr - 1, list, rullo, chosen | (one << i), i + 1);
-    comb(size, curr, list, rullo, chosen, i + 1);  /* or don't choose it, go to next */
-}
- 
-void insertFirst(int value) {
-    struct node* temp = (struct node*) malloc(sizeof(struct node));
-    temp->value = value;
-    temp->next = head;
-    head = temp;
-}
-
-void deleteFirst() { head = head->next; }
-
-void printList() {
-    struct node *temp = head;
+/********************************************************
+ * INPUTS:
+ *      NONE
+ *
+ * OUTPUTS:
+ *      NONE
+ *
+ * RETURN:
+ *      NONE
+ *
+ * DeSCRIPTION:
+ *      Prints what is currently in the list
+ ********************************************************/
+static void printList( void ) {
+    set_s *temp = head;
 
     printf("Found combination: [ ");
     while(temp != NULL) {
@@ -87,16 +127,146 @@ void printList() {
     printf("]\n");
 }
 
-int sum() {
-    int sum = 0;
-    struct node *temp;
-    for(temp = head; temp != NULL; temp = temp->next) { sum += temp->value; }
+/********************************************************
+ * INPUTS:
+ *      NONE
+ *
+ * OUTPUTS:
+ *      NONE
+ *
+ * RETURN:
+ *      sum - All the values in the linked list added up
+ *
+ * DeSCRIPTION:
+ *      See RETURN
+ ********************************************************/
+static uint32_t sum( void ) {
+    uint32_t sum = 0;
+    set_s *temp;
+
+    for(temp = head; temp != NULL; temp = temp->next) {
+        sum += temp->value;
+    }
+
     return sum;
 }
 
-int length() {
-    int length = 0;
-    struct node *temp;
-    for(temp = head; temp != NULL; temp = temp->next) { length++; }
-    return length;
+/********************************************************
+ * INPUTS:
+ *        size - Size of the array
+ *        need - How much bits needed to find combination
+ *        list - Complete valid set
+ *       rullo - Number that the set should equal
+ *      chosen - Using bits to indicate what's chosen
+ *          at - Current index location
+ *
+ * OUTPUTS:
+ *      NONE
+ *
+ * RETURN:
+ *      NONE
+ *
+ * DeSCRIPTION:
+ *      Find all the combinations from the list
+ *
+ *      Implementing the combinatorics code from Rosetta Code
+ *      http://rosettacode.org/wiki/Combinations#C.2B.2B
+ ********************************************************/
+static void comb(const uint32_t size,
+                 const uint32_t need,
+                 const uint32_t list[],
+                 const uint32_t rullo,
+                 const unsigned long chosen,
+                 uint32_t at) {
+    if (size < need + at) {
+        return;
+    }
+
+    if (!need) {
+        for (at = 0; at < size; at++) {
+            if (chosen & (one << at)) {
+                insertFirst(list[at]);
+            }
+        }
+
+        if (sum() == rullo) {
+            printList();
+        }
+
+        while (NULL != head) {
+            deleteFirst();
+        }
+        free(head);
+        return;
+    }
+
+    // If we choose the current item, "or" (|) the bit to mark it so.
+    comb(size, need - 1, list, rullo, chosen | (one << at), at + 1);
+
+    // Or don't choose it, go to next
+    comb(size, need, list, rullo, chosen, at + 1);
+}
+
+int main(int argc, char **argv) {
+    bool ok = true;
+    if(argc < 3) {
+        printf("Error: No arguments given: %s [rullo number] [set of row/column]\n", argv[0]);
+        ok = false;
+    }
+
+    uint32_t i = 0;
+    uint32_t rullo = 0;
+    if ( ok && (! ok_strtol(argv[1], &rullo)) ) {
+        ok = false;
+        printf("Error: Failed to retrieve rullo number - rullo string(%s)\n", argv[1]);
+    }
+
+    if ( ok ) {
+        for(i = 2; i < argc; i++) {
+            uint32_t set = 0;
+            if ( ok_strtol(argv[i], &set) ) {
+                if ( set <= rullo ) {
+                    insertFirst(set);
+                }
+            } else {
+                ok = false;
+                printf("Error: Failed to retrieve set number - set(%s)\n", argv[i]);
+                break;
+            }
+        }
+    }
+
+    if ( ok ) {
+        /*
+         * Found all valid set numbers. Create
+         * an array and store them. Then delete
+         * the linked list.
+         */
+        uint32_t const SIZE = set_length;
+        uint32_t list[SIZE];
+        memset(list, 0, SIZE);
+        for(i = 0; i < SIZE; i++) {
+            list[i] = head->value;
+            head = head->next;
+        }
+
+        while (NULL != head) {
+            deleteFirst();
+        }
+        free(head);
+
+        printf("Rullo number: %d\n", rullo);
+        printf("Accepted set: [ ");
+        for(i = 0; i < SIZE; i++) {
+            printf("%d ", list[i]);
+        }
+
+        printf("]\n==========================================\n");
+        for(i = 0; i < (SIZE + 1); i++) {
+            comb(SIZE, i, list, rullo, 0, 0);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
